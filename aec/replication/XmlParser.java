7aa0aabb62a51a46471d8b33463734f71b6fb329
nesrineD <nesrine.doghri1@gmail.com>
2015-06-14 14:53:18 +0200

    store xml values in a Hashmap

--- aec/replication/XmlParser.java
+++ aec/replication/XmlParser.java
@@ -1,16 +1,24 @@
 package aec.replication;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.List;
 
 public class XmlParser {
 	
+	/**
+	 * @param argv
+	 */
 	public static void main(String argv[]) {
 
 		try {
 			
 			File file = new File("src/resources/ReplicationPath.xml");
-			Parsing parser = new Parsing();
-			parser.ParseXML(file);
+			ParseXML parse = new ParseXML();
+			parse.parseXML(file);
+			HashMap<String, List<String>> map = new HashMap<String, List<String>>();
+			map = parse.getMap();
+			parse.readMap(map);
 
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
